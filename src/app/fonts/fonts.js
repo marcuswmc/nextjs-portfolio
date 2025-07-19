1413cf871c4f3f40bfd7c7c6e4472c77f93f5a75
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-07-19 13:09:03 +0100

    first commit: Create Navbar and hero

---
+++ src/app/fonts/fonts.js
@@ -0,0 +1,74 @@
+import localFont from "next/font/local";
+
+export const amiamieRegular = localFont({
+  src: "./amiamie-regular.woff2",
+  weight: "400",
+  style: "normal",
+  variable: "--font-normal",
+  display: "swap",
+});
+
+export const amiamieItalic = localFont({
+  src: "./amiamie-italic.woff2",
+  weight: "400",
+  style: "italic",
+  variable: "--font-italic",
+  display: "swap",
+});
+
+export const amiamieLight = localFont({
+  src: "./amiamie-light.woff2",
+  weight: "300",
+  style: "normal",
+  variable: "--font-light",
+  display: "swap",
+});
+
+export const amiamieLightItalic = localFont({
+  src: "./amiamie-light-italic.woff2",
+  weight: "300",
+  style: "italic",
+  variable: "--font-light-italic",
+  display: "swap",
+});
+
+export const amiamieBlack = localFont({
+  src: "./amiamie-black.woff2",
+  weight: "900",
+  style: "normal",
+  variable: "--font-black",
+  display: "swap",
+});
+
+export const amiamieBlackItalic = localFont({
+  src: "./amiamie-black-italic.woff2",
+  weight: "900",
+  style: "italic",
+  variable: "--font-black-italic",
+  display: "swap",
+});
+
+// Amiamie-Round
+export const amiamieRoundRegular = localFont({
+  src: "./amiamie-regular-round.woff2",
+  weight: "400",
+  style: "normal",
+  variable: "--font-round",
+  display: "swap",
+});
+
+export const amiamieRoundBlack = localFont({
+  src: "./amiamie-black-round.woff2",
+  weight: "900",
+  style: "normal",
+  variable: "--font-round-black",
+  display: "swap",
+});
+
+export const amiamieRoundBlackItalic = localFont({
+  src: "./amiamie-black-italic-round.woff2",
+  weight: "900",
+  style: "italic",
+  variable: "--font-round-black-italic",
+  display: "swap",
+});
